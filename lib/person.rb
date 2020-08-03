a8fa1817751bff84ba839c2e8d38585aab31c256
Anthony Lepore <violacellonyc@gmail.com>
2020-08-03 18:24:16 +0000

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,14 @@
+class Person 
+  def name=(the_name)
+    @name = the_name
+  end
+  def name
+    @name
+  end
+  def job=(persons_job)
+    @job = persons_job
+  end
+  def job
+    @job
+  end
+end
